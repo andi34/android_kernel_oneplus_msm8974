b64aa3c03efb751c606c9688fb0baf91fb680c78
Michael Bestas <mikeioannina@gmail.com>
2017-08-19 15:33:53 +0200

    Fix compilation after Merge branch 'deprecated/android-3.4' of https://android.googlesource.com/kernel/common
    Change-Id: I15ebf14ee5dd1cf43cbd702d54ba8e8364028eb7

--- arch/arm/mm/mmu.c
+++ arch/arm/mm/mmu.c
@@ -533,14 +533,6 @@ static void __init build_mem_type_table(void)
 	}
 
 #ifndef CONFIG_ARM_LPAE
-        /*
-         * We don't use domains on ARMv6 (since this causes problems with
-         * v6/v7 kernels), so we must use a separate memory type for user
-         * r/o, kernel r/w to map the vectors page.
-         */
-        if (cpu_arch == CPU_ARCH_ARMv6)
-                vecs_pgprot |= L_PTE_MT_VECTORS;
-
 	/*
 	 * Check is it with support for the PXN bit
 	 * in the Short-descriptor translation table format descriptors.
